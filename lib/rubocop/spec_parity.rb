e881bf734f38ae6e03ee9ebe822e243f000d563b
Povilas Jurcys <po.jurcys@gmail.com>
2026-01-15 17:50:51 +0200

    Initial commit

---
+++ lib/rubocop/spec_parity.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "spec_parity/version"
+
+module Rubocop
+  module SpecParity
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
